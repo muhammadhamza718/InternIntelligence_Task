ff1d9c975b3c1c337f67f4613843a70c40467fe4
muhammadhamza718 <mhamza77188@gmail.com>
2025-03-29 21:37:22 +0500

    Task_01

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,9 +1,23 @@
-import ResetPasswordFormWrapper from "@/components/reset-password-wrapper";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { redirect } from "next/navigation";
+import ResetPasswordForm from "@/components/reset-password-form";
 
-export default function ResetPasswordPage() {
+// @ts-ignore
+export default async function ResetPasswordPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const token = searchParams.token as string;
+
+  // If no token is provided, redirect to home
+  if (!token) {
+    redirect("/");
+  }
+
   return (
     <div className="min-h-screen bg-slate-50">
-      <ResetPasswordFormWrapper />
+      <ResetPasswordForm token={token} />
     </div>
   );
 }
